45d1bb83dd7e29855cd8f56c28aa85e42007a993
lrodrin <laura.rodriguez@bsc.es>
2020-03-11 11:06:13 +0100

    refactor packed cwl method. added method to create input and output list from CWL

--- lib/pack_cwl.py
+++ lib/pack_cwl.py
@@ -1,80 +1,50 @@
-# import subprocess
-#
-# retval = subprocess.run(["cwltool", "basic_example.cwl", "input_basic_example.yml"])
-# print(retval)
-
 import json
 import os
-from re import match
-from shutil import copyfile
 
-from distutils.version import StrictVersion
-from pkg_resources import get_distribution
+from cwltool.load_tool import fetch_document
+from cwltool.main import print_pack
+from cwltool.load_tool import resolve_and_validate_document
+
+os.chdir(os.path.dirname(__file__))
 
 
-def pack_cwl(cwl_path):
+def resolve_and_validate(cwl_path):
     """
-    cwltool needs to be imported on demand since repeatedly calling functions on a document named with same name
-    caused errors.
 
-    :param cwl_path:
-    :type cwl_path: str
     """
-    from cwltool.load_tool import fetch_document
-    from cwltool.main import print_pack
-    cwltool_version = get_distribution("cwltool").version
-    if StrictVersion(cwltool_version) > StrictVersion("1.0.20181201184214"):
-        from cwltool.load_tool import resolve_and_validate_document
+    try:
         loadingContext, workflowobj, uri = fetch_document(cwl_path)
         loadingContext.do_update = False
         loadingContext, uri = resolve_and_validate_document(loadingContext, workflowobj, uri)
         processobj = loadingContext.loader.resolve_ref(uri)[0]
         packed_cwl = json.loads(print_pack(loadingContext.loader, processobj, uri, loadingContext.metadata))
-    else:
-        from cwltool.load_tool import validate_document
-        document_loader, workflowobj, uri = fetch_document(cwl_path)
-        document_loader, _, processobj, metadata, uri = validate_document(document_loader, workflowobj, uri, [], {})
-        packed_cwl = json.loads(print_pack(document_loader, processobj, uri, metadata))
-    return packed_cwl
+        return packed_cwl
+
+    except Exception as error:
+        errstr = "Unable to pack CWL. ERROR: {}".format(error)
+        raise Exception(errstr)
 
 
-def import_cwl(wf_path, name):
-    cwl_filename = "{}.{}".format(name, "cwl")
+def pack_cwl(wf_path):
+    """
 
-    cwl_file_path = os.path.join(os.path.curdir, cwl_filename)
+    """
+    cwl_filename = os.path.basename(wf_path)
+    cwl_file_path = os.path.join(os.path.curdir, cwl_filename.split(".")[0])
     if not os.path.exists(cwl_file_path):
-        try:
-            os.makedirs(cwl_file_path)
-        except IOError as error:
-            errstr = "ERROR: Unable to create intermediate directories. Error: {}".format(error)
-            raise Exception(errstr)
+        os.makedirs(cwl_file_path)
 
     try:
-        packed_cwl = pack_cwl(wf_path)
-
-    except Exception as e:
-        raise Exception("The loaded CWL document is not valid. Error: {}".format(str(e)))
-
-    # temp_dir = make_temp_dir()
-    packed_cwl_path = os.path.join(cwl_file_path, "packed.cwl")
-    try:
+        packed_cwl = resolve_and_validate(wf_path)
+        packed_cwl_path = os.path.join(cwl_file_path, "pack_" + cwl_filename)
         with open(packed_cwl_path, 'w') as cwl_file:
             json.dump(packed_cwl, cwl_file)
-    except Exception as e:
-        raise AssertionError("Could not write CWL file. Error: %s" % e)
-    # job_templ_path = os.path.join(temp_dir, "job_templ.xlsx")
-    # generate_job_template_from_cwl(
-    #     workflow_file=wf_temp_path,
-    #     wf_type="CWL",
-    #     output_file=job_templ_path,
-    #     show_please_fill=True
-    # )
-    cwl_document = read_config_from_cwl_file(packed_cwl_path)
-    print(cwl_document)
-    #copyfile(wf_temp_path, wf_target_path)
-    # job_templ_target_path = get_path("job_templ", wf_target=cwl_filename)
-    # copyfile(job_templ_path, job_templ_target_path)
-    # rmtree(temp_dir)
+
+    except Exception as error:
+        errstr = "Unable to create CWL packed. ERROR: {}".format(error)
+        raise Exception(errstr)
+
+    # cwl_document = read_config_from_cwl_file(packed_cwl_path)
 
 
 def read_config_from_cwl_file(cwl_file):
@@ -99,7 +69,7 @@ def read_config_from_cwl_file(cwl_file):
     try:
         cwl_document = load_tool(cwl_file, loadingContext)
     except AssertionError as e:
-        raise AssertionError( print_pref + "failed to read cwl file \"" + cwl_file + "\": does not exist or is invalid")
+        raise AssertionError(print_pref + "failed to read cwl file \"" + cwl_file + "\": does not exist or is invalid")
     inp_records = cwl_document.inputs_record_schema["fields"]
     outp_records = cwl_document.outputs_record_schema["fields"]
     print(inp_records)
@@ -178,17 +148,7 @@ def read_config_from_cwl_file(cwl_file):
 
 
 if __name__ == '__main__':
-    # pack = pack_cwl("/home/laura/PycharmProjects/vre_cwl_executor/cwl_wrapper_test/tests/data"
-    #                 "/samtools_split.cwl")
-    #
-    # print(pack)
-    #
-    # files = fetch_files_in_dir(
-    #     dir_path="/home/laura/PycharmProjects/vre_cwl_executor/cwl_wrapper_test/tests/data",
-    #     file_exts=["cwl"],
-    #     ignore_subdirs=True)
-    # print(files)
-    #
-    import_cwl("https://raw.githubusercontent.com/lrodrin/vre_cwl_executor/master/cwl_wrapper_test/tests/data/workflows/basic_example.cwl", "basic_example")
-    # import_cwl("https://raw.githubusercontent.com/CompEpigen/ATACseq_workflows/1.2.0/CWL/workflows/ATACseq.cwl", "test")
-
+    basic_cwl_path = "/home/laura/PycharmProjects/vre_cwl_executor/tests/basic/data/workflows/basic_example_v2.cwl"
+    cwl_path = "/home/laura/PycharmProjects/vre_cwl_executor/tests/basic/data/workflows/rp2-to-rp2path_modified.cwl"
+    # resolve_and_validate(basic_cwl_path)
+    pack_cwl(basic_cwl_path)
---
+++ lib/read_cwl.py
@@ -0,0 +1,19 @@
+from cwltool.load_tool import load_tool
+from cwltool.workflow import default_make_tool
+
+basic_cwl_path = "/home/laura/PycharmProjects/vre_cwl_executor/tests/basic/data/workflows/basic_example_v2.cwl"
+cwl_path = "/home/laura/PycharmProjects/vre_cwl_executor/tests/basic/data/workflows/rp2-to-rp2path_modified.cwl"
+
+cwl_document = load_tool(cwl_path)
+inputs_record_schema = cwl_document.inputs_record_schema["fields"]
+outputs_record_schema = cwl_document.outputs_record_schema["fields"]
+print("INPUT RECORD SCHEMA: {}".format(inputs_record_schema))
+print("OUTPUT RECORD SCHEMA: {}".format(outputs_record_schema))
+
+for inputs in inputs_record_schema:
+    print(inputs)
+
+for outputs in outputs_record_schema:
+    print(outputs)
+    # for item in outputs.items():
+    #     print(item)
