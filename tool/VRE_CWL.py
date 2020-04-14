1cde07e4a858ac99639787f4ee669daef5e25d69
lrodrin <lrodrin@gmail.com>
2020-04-14 13:02:00 +0200

    update config and in_metadata like VRE

--- tool/VRE_CWL.py
+++ tool/VRE_CWL.py
@@ -157,7 +157,8 @@ class WF_RUNNER(Tool):
             logger.fatal(errstr)
             raise Exception(errstr)
 
-    def create_output_metadata(self, input_metadata, output_metadata):
+    @staticmethod
+    def create_output_metadata(input_metadata, output_metadata):
         """
         Create returned output metadata from input metadata and output metadata from output files.
 
@@ -175,10 +176,7 @@ class WF_RUNNER(Tool):
                 meta = Metadata()
 
                 # Set file_path for output files
-                file_path = output_file["file"].get("file_path", None)
-                if os.path.isfile(file_path):  # if file_path is a file
-                    execution_path = os.path.abspath(self.configuration.get('execution', '.'))
-                    meta.file_path = os.path.join(execution_path, file_path)
+                meta.file_path = output_file["file"].get("file_path", None)
 
                 # Set data and file types of output_file
                 meta.data_type = output_file["file"].get("data_type", None)
